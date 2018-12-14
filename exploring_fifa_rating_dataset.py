d2ccf276b86cf6fb8c151f47ffdd43283e75d328
Roald Schuring <roald.schuring@hotmail.com>
2018-12-13 21:40:11 -0600

    updated fifa_rating_dataset_file

--- exploring_fifa_rating_dataset.py
+++ exploring_fifa_rating_dataset.py
@@ -6,15 +6,12 @@ compressed_fifa_ratings = pd.read_csv('compressed_player_rating_dataset.csv', en
 compressed_fifa_ratings.set_index('player_url')
 parsed_positions = compressed_fifa_ratings['position'].str.split(' ', expand=True)
 parsed_positions.columns = ['main_position', 'position_2', 'position_3', 'position_4']
-
 compressed_fifa_ratings = pd.concat([compressed_fifa_ratings, parsed_positions], axis=1)
 
 just_overall_ratings = compressed_fifa_ratings[['player_url', 'age', 'main_position', 'overall']].drop_duplicates()
 just_overall_ratings['age_next_year'] = just_overall_ratings['age'].apply(lambda x: x+1)
-
 year_over_year_ratings = pd.merge(just_overall_ratings, just_overall_ratings, left_on=['player_url', 'age_next_year'],
                                   right_on=['player_url', 'age'], how='left').drop_duplicates()
-
 year_over_year_ratings = year_over_year_ratings[['player_url', 'main_position_x', 'age_x', 'age_next_year_x', 'overall_x', 'overall_y']]
 year_over_year_ratings.columns = ['player_url', 'main_position', 'age', 'age_next_year', 'overall', 'next_year_overall']
 year_over_year_ratings.dropna(inplace=True)
